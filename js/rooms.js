715a5d5b7d65b730c92d060ba3d28c8c1c3998da
callmetarush <tarush.nagpal7@gmail.com>
2018-01-06 22:10:01 +0530

    changes

--- js/rooms.js
+++ js/rooms.js
@@ -4,11 +4,21 @@ $(document).ready(function(){
         alert("Data: " + data + "\nStatus: " + status);
     });
 
-        
-    $("button").click(function(){
+    rooms = data.rooms;
+
+    for (i = 0; i < rooms.length; i++) {
+        var index = i;
+        var roomno = rooms[i];
+        var markup = "<tr><td>" + index + "</td><td>" + roomno + "</td><td><span class='glyphicon glyphicon-remove'></span></td></tr>";
+        $("table tbody").append(markup);
+    
+    }   
+
+    $('#thetable').find('tr').click( function(){
+        click_index = ($(this).index()+1);
         $.delete("http://pgolecha.me:8383/hotel/delete_key",
         {
-          "room":$('#room_number').val()
+          "room": rooms[click_index]
         },
         function(data,status){
             alert("Data: " + data + "\nStatus: " + status);
