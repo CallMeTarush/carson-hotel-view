c952106bd42113c9fb22b148a1e6f90524126bb9
callmetarush <tarush.nagpal7@gmail.com>
2018-01-18 17:37:47 +0530

    changes

--- js/rooms.js
+++ js/rooms.js
@@ -1,36 +1,38 @@
-
 var rooms; 
 
 $(document).ready(function(){
-    
+    getRooms();   
+});
+
+function getRooms() {
     $.get("http://pgolecha.me:8383/hotel/view_rooms",function(data,status) {
-                rooms = data.rooms;
-                 for (i = 0; i < rooms.length; i++) {
+        rooms = data.rooms;
+        for (i = 0; i < rooms.length; i++) {
             var index = i;
             var roomno = rooms[i].room;
             var markup = "<tr><td>" + index + "</td><td>" + roomno + "</td><td><button onClick='deleteRoom(" + index + ")'><span class='glyphicon glyphicon-remove'></span></button></td></tr>";
-            $("table tbody").append(markup);        
+            $("table tbody").append(markup);
         }
-            });
-
-       
-
-    
-});
+    });    
+}
 
 function deleteRoom(click_index){
     console.log(rooms[click_index].room);
+    
+    var data_to_send = { 
+        'room':rooms[click_index].room
+    };
+
     $.ajax({
         url: 'http://pgolecha.me:8383/hotel/delete_key',
         type: 'DELETE',
-        data: {
-            "room": rooms[click_index].room
-        },
-        contentType:'application/json',  
+        data: JSON.stringify(data_to_send),
         dataType: 'json',               
         success: function(result) {alert("deleted!")},
         error: function(result){"error!"}
     });
+
+    getRooms();
 }
 
 function checkin() {
@@ -48,4 +50,6 @@ function checkin() {
             alert(msg);
         }
     });
+
+    getRooms();
 }
