16f037a50316f4e604c91ddbd87939deeb7754e8
callmetarush <tarush.nagpal7@gmail.com>
2018-02-19 17:24:06 +0000

    cganges

--- js/chat-interface.js
+++ js/chat-interface.js
@@ -1,3 +1,5 @@
+window.width = $(window).width();
+
 
 window.current = '';
 var hotelDatabase = firebase.database().ref('hotel/1');
@@ -7,6 +9,7 @@ function init() {
 
   hotelDatabase.once('value').then(function(snapshot) {
 
+    
     document.getElementById("roomNumbers").innerHTML='';
     document.getElementById("conversation").innerHTML='';
 
@@ -14,7 +17,6 @@ function init() {
     user_id_list = snap.user_list;
     len = user_id_list.length;
 
-    
     for (var i = len-1 ; i >= 0 ; i--) {
         
       x = snap.user_list[i];
@@ -61,27 +63,37 @@ function init() {
 }
 function loadChat(counter_details) {
   
-  $('.selected').removeClass('selected');
-  $('#chat-selected-'+ counter_details).addClass('selected');
-
-  document.getElementById("conversation").innerHTML='';
-  
-  x = snap.user_list[counter_details];
-  
-  current = x;
-  user_details = snap[x];
-  chats = user_details.chats.reception;
-  
-  console.log(user_details.room_number);
-  document.getElementById("display-room").innerHTML=user_details.room_number;
-
-  for (const j of Object.keys(chats)) {
-    var chat_timestamp = chats[j].datetime;
-    
-    addMessageText(chats[j].message,chat_timestamp,chats[j].sender);
-    
+  if(width<768) {
+    $('#the-left-side').removeClass('col-xs-12');
+    $("#the-left-side").css("display", "none"); 
+    $('#the-right-side').addClass('col-xs-12');
+    $('#the-right-side').css("display","block");
+    console.log("fuck");
   }
+  else {
+    $('.selected').removeClass('selected');
+    $('#chat-selected-'+ counter_details).addClass('selected');
 
+    document.getElementById("conversation").innerHTML='';
+    
+    x = snap.user_list[counter_details];
+    
+    current = x;
+    user_details = snap[x];
+    chats = user_details.chats.reception;
+    
+    console.log(user_details.room_number);
+    document.getElementById("display-room").innerHTML=user_details.room_number;
+
+    for (const j of Object.keys(chats)) {
+   
+      var chat_timestamp = chats[j].datetime;
+      addMessageText(chats[j].message,chat_timestamp,chats[j].sender);      
+   
+    }
+
+  }
+  
   
   scrollDown();
 
@@ -90,10 +102,17 @@ function loadChat(counter_details) {
 
 function addMessageText(message,timestamp,sender) {
 
-  var chat_time = timestamp.substring(11,16);
-  var chat_day = timestamp.substring(8,10);
-  var chat_month = timestamp.substring(5,7);
+  
+  var myDate = new Date(timestamp);
+  var formatedTime=myDate.toJSON();
+  
+  
+  var chat_time = formatedTime.substring(11,16);
+  var chat_day = formatedTime.substring(8,10);
+  var chat_month = formatedTime.substring(5,7);
 
+  console.log(chat_time,chat_day,chat_month);
+  
   if(sender == 0) {
     
     document.getElementById("conversation").innerHTML+=`
@@ -134,10 +153,8 @@ function sendMessage() {
 
   // console.log(current);
 
-  
-
-  var d = new Date();
-  var date = d.toISOString();
+  console.log();
+  var date = firebase.database.ServerValue.TIMESTAMP;
   var message_send = document.getElementById("comment").value;
   
   if (message_send=="" || message_send==undefined) {
@@ -146,14 +163,13 @@ function sendMessage() {
 
   else {
     var msgObject = { datetime: date, message: message_send, sender: 1};
-    // console.log(msgObject,current);
-    
+    console.log(msgObject,current);
+
     firebase.database().ref('/hotel/1/'+ current +'/chats/reception/').push(msgObject);
 
     user_details_reload = snap[current];
     chats= user_details_reload.chats.reception;
 
-    
     
     document.getElementById("conversation").innerHTML='';
     // console.log(chats);
@@ -181,41 +197,46 @@ function scrollDown() {
 }
 
 init();
-
-
+function backPressed() {
+  $('#the-right-side').removeClass('col-xs-12');
+  $("#the-right-side").css("display", "none"); 
+  $('#the-left-side').addClass('col-xs-12');
+  $('#the-left-side').css("display","block");
+  
+  console.log("fuck");
+}
 
 
 
 hotelDatabase.on('value', function(snapshot) {
-    document.getElementById("roomNumbers").innerHTML='';
-    
-    window.snap = snapshot.val();
-    user_id_list = snap.user_list;
-    len = user_id_list.length;
+  document.getElementById("roomNumbers").innerHTML='';
+  
+  window.snap = snapshot.val();
+  user_id_list = snap.user_list;
+  len = user_id_list.length;
 
+  
+  for (var i = len-1 ; i >= 0 ; i--) {
     
-    for (var i = len-1 ; i >= 0 ; i--) {
-      
-      document.getElementById("roomNumbers").innerHTML+='';
+    document.getElementById("roomNumbers").innerHTML+='';
 
-      x = snap.user_list[i];
-      user_details = snap[x];
-      chats = user_details.chats.reception;
-      
-      document.getElementById("roomNumbers").innerHTML+=`
-      <div class="row sideBar-body" onClick="loadChat(`+i+`)" id="chat-selected-` + i +`">          
-        <div class="col-sm-9 col-xs-9 sideBar-main">
-          <div class="row">
-            <div class="col-sm-8 col-xs-8 sideBar-name">
-              <span class="name-meta">
-                ` + user_details.room_number + `
-              </span>
-            </div>
-            
+    x = snap.user_list[i];
+    user_details = snap[x];
+    chats = user_details.chats.reception;
+    
+    document.getElementById("roomNumbers").innerHTML+=`
+    <div class="row sideBar-body" onClick="loadChat(`+i+`)" id="chat-selected-` + i +`">          
+      <div class="col-sm-9 col-xs-9 sideBar-main">
+        <div class="row">
+          <div class="col-sm-8 col-xs-8 sideBar-name">
+            <span class="name-meta">
+              ` + user_details.room_number + `
+            </span>
           </div>
+          
         </div>
-      </div>`;
-        
-    }
+      </div>
+    </div>`;
+      
+  }
 });
-
